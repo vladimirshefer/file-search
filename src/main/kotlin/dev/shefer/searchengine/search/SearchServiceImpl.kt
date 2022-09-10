e589822c755facbdf432e62121a31e8fa2b2a984
Vladimir Shefer <quameu@gmail.com>
2022-09-10 04:47:44 +0400

    Fix SearchServiceImpl

--- src/main/kotlin/dev/shefer/searchengine/search/SearchServiceImpl.kt
+++ src/main/kotlin/dev/shefer/searchengine/search/SearchServiceImpl.kt
@@ -4,7 +4,6 @@ import dev.shefer.searchengine.Analyzer
 import dev.shefer.searchengine.LineLocation
 import dev.shefer.searchengine.analyze
 import dev.shefer.searchengine.engine.service.TokenService
-import dev.shefer.searchengine.tokenRepository
 import org.springframework.stereotype.Service
 
 @Service
@@ -16,12 +15,12 @@ class SearchServiceImpl(
     override fun search(query: String): List<LineLocation> {
         val queryTokens = analyzer.analyze(query)
 
-        val searchCandidates = tokenRepository.findLinesByToken(queryTokens[0])
+        val searchCandidates = tokenService.findLinesByToken(queryTokens[0])
         val result = ArrayList<LineLocation>()
         for (searchCandidate in searchCandidates) {
             var allExist = true
             for (queryToken in queryTokens) {
-                val checkExists = tokenRepository.checkExists(searchCandidate, queryToken)
+                val checkExists = tokenService.checkExists(searchCandidate, queryToken)
                 if (!checkExists) {
                     allExist = false
                     break
