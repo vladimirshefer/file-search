beebf4483120e4648e762eff1d38bfbc4ea6cf66
Vladimir Shefer <quameu@gmail.com>
2022-09-12 02:45:55 +0400

    TokenFilter: add doc

--- src/main/kotlin/dev/shefer/searchengine/engine/filter/TokenFilter.kt
+++ src/main/kotlin/dev/shefer/searchengine/engine/filter/TokenFilter.kt
@@ -1,5 +1,16 @@
 package dev.shefer.searchengine.engine.filter
 
+/**
+ * Allows to change tokens before being indexed.
+ * See LowercaseTokenFilter, which lowercases all tokens
+ * to make them case insensitive during the search.
+ */
 interface TokenFilter {
+
+    /**
+     * Return either same token or token replacement or null.
+     * Null means the token should be skipped and not indexed.
+     */
     fun filter(token: String) : String?
+
 }
