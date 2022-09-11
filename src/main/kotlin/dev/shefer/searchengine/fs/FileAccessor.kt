a2d50202ca2d09251efedc4bea66b892e536e292
Vladimir Shefer <quameu@gmail.com>
2022-09-12 02:00:06 +0400

    Fix NPE in FileAccessor if file changed

--- src/main/kotlin/dev/shefer/searchengine/fs/FileAccessor.kt
+++ src/main/kotlin/dev/shefer/searchengine/fs/FileAccessor.kt
@@ -2,18 +2,17 @@ package dev.shefer.searchengine.fs
 
 import dev.shefer.searchengine.engine.dto.LineLocation
 import java.io.RandomAccessFile
-import java.nio.file.Paths
 
 class FileAccessor {
 
     fun getLine(lineLocation: LineLocation): String {
         val filename = lineLocation.fileLocation.directoryPath + "/" + lineLocation.fileLocation.fileName
-        val path = Paths.get(filename)
         val channel = RandomAccessFile(filename, "r")
         for (i in 0 until lineLocation.lineIndex) {
             channel.readLine()
         }
         val line = channel.readLine()
+            ?: throw IllegalStateException("Could not read line from file. Has the file been changed or deleted?")
         return line;
     }
 
