a6482471f2774769367e57f34f092d9c2ad96d4d
Vladimir Shefer <quameu@gmail.com>
2022-09-12 21:07:12 +0400

    Add docs to LowercaseTokenFilter

--- src/main/kotlin/dev/shefer/searchengine/engine/filter/LowercaseTokenFilter.kt
+++ src/main/kotlin/dev/shefer/searchengine/engine/filter/LowercaseTokenFilter.kt
@@ -1,5 +1,9 @@
 package dev.shefer.searchengine.engine.filter
 
+/**
+ * Replaces all uppercase symbols in token to lowercase.
+ * Basically, used to make search case-insensitive.
+ */
 class LowercaseTokenFilter : TokenFilter {
     override fun filter(token: String): String {
         return token.lowercase()
