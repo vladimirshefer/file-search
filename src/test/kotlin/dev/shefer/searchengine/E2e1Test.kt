dd1e4b485e5336071c1acdb7b88fae61553669f2
Vladimir Shefer <quameu@gmail.com>
2022-09-12 06:02:12 +0400

    Add more test cases and data

--- src/test/kotlin/dev/shefer/searchengine/E2e1Test.kt
+++ src/test/kotlin/dev/shefer/searchengine/E2e1Test.kt
@@ -18,7 +18,10 @@ class E2e1Test : E2eBaseTest() {
         searchEngine.saveIndex()
         verifyIndexFiles()
 
+        verifySearch("hello, hello")
+        verifySearch("Your heart's been aching, but you're too shy to say it (say it)")
         verifySearch("gonna let you down")
+        verifySearch("We all live in a yellow submarine")
         verifySearch("gonna make")
         verifySearch("gonna")
     }
