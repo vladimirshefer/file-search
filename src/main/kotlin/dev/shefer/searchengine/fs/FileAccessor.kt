bd202f249579f86585a5529dd6a5be38f43e5b5d
Vladimir Shefer <quameu@gmail.com>
2022-09-12 21:07:42 +0400

    Add docs to FileAccessor

--- src/main/kotlin/dev/shefer/searchengine/fs/FileAccessor.kt
+++ src/main/kotlin/dev/shefer/searchengine/fs/FileAccessor.kt
@@ -5,15 +5,18 @@ import java.io.RandomAccessFile
 
 class FileAccessor {
 
+    /**
+     * Reads the turns the line of the file.
+     * TODO: Optimize reads (with RandomAccessFile?)
+     */
     fun getLine(lineLocation: LineLocation): String {
         val filename = lineLocation.fileLocation.directoryPath + "/" + lineLocation.fileLocation.fileName
         val channel = RandomAccessFile(filename, "r")
         for (i in 0 until lineLocation.lineIndex) {
             channel.readLine()
         }
-        val line = channel.readLine()
+        return channel.readLine()
             ?: throw IllegalStateException("Could not read line from file. Has the file been changed or deleted?")
-        return line;
     }
 
 }
