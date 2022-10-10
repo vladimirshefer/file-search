04e3598b081580bb01c114455a3903d61f00beaa
Vladimir Shefer <quameu@gmail.com>
2022-10-11 01:13:26 +0400

    ui: extract header

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -2,17 +2,15 @@ import React from 'react';
 import 'App.css';
 import FilesPage from 'pages/FilesPage';
 import FileEditPage from "pages/FileEditPage";
-import {BrowserRouter, Link, Navigate, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
+import Header from "components/Header";
 
 function App() {
     return (
         <div className='App'>
             <BrowserRouter>
                 <React.StrictMode>
-                    <nav>
-                        <Link to={"/"}>Home</Link> |
-                        <Link to={"/files"}>File tree</Link>
-                    </nav>
+                    <Header/>
                     <Routes>
                         <Route path='/' element={<Navigate to="/files"/>}/>
                         <Route path="/files/*" element={<FilesPage/>}/>
---
+++ ui/src/components/Header/index.tsx
@@ -0,0 +1,29 @@
+import {Link} from "react-router-dom";
+import React from "react";
+import "styles/Header.css"
+
+function Header() {
+    return <>
+        <nav className={"header"}>
+            <div className={"header_logo"}>
+                <span className={"header_logo_text"}>#</span>
+            </div>
+            <NavItem to={"/"} text={"Home"}/>
+            <NavItem to={"/files"} text={"Files"}/>
+        </nav>
+    </>
+}
+
+function NavItem(props: {
+    to: string,
+    text: string
+}) {
+    return <Link
+        to={props.to}
+        className={"header_nav-item"}
+    >
+        {props.text}
+    </Link>
+}
+
+export default Header
