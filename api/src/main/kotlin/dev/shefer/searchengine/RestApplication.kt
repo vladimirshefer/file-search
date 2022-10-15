6a66854c6dc6d978ae902888a253f65efc04faea
Vladimir Shefer <quameu@gmail.com>
2022-10-16 01:40:45 +0400

    Define bean MediaOptimizationManager

--- api/src/main/kotlin/dev/shefer/searchengine/RestApplication.kt
+++ api/src/main/kotlin/dev/shefer/searchengine/RestApplication.kt
@@ -1,10 +1,27 @@
 package dev.shefer.searchengine
 
+import dev.shefer.searchengine.optimize.MediaOptimizationManager
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import java.nio.file.Path
 
 @SpringBootApplication
-class RestApplication
+class RestApplication {
+    @Bean
+    fun mediaOptimizationManager(
+        @Value("\${app.sourceMediaRootPath}")
+        sourceMediaRootPath: String,
+        @Value("\${app.optimizedMediaRootPath}")
+        optimizedMediaRootPath: String
+    ): MediaOptimizationManager {
+        return MediaOptimizationManager(
+            Path.of(sourceMediaRootPath),
+            Path.of(optimizedMediaRootPath)
+        )
+    }
+}
 
 fun main() {
     runApplication<RestApplication>()
