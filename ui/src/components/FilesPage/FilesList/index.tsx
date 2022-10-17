9a35e81dd6851dbc90603df4fa4b548d8ff51daa
Vladimir Shefer <quameu@gmail.com>
2022-10-18 01:33:04 +0400

    ui: FilesList: Fix: Show optimized name instead of placeholder

--- ui/src/components/FilesPage/FilesList/index.tsx
+++ ui/src/components/FilesPage/FilesList/index.tsx
@@ -22,7 +22,7 @@ export function FilesList(
             {
                 (!!file.optimized) ?
                     <span className={"file-info_name-optimized"}>
-                        file.optimized.name
+                        {file.optimized.name}
                     </span>
                     : null
             }
