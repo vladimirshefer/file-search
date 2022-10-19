563a49acab3c00ec230c136f4cdf818355b2149d
Vladimir Shefer <quameu@gmail.com>
2022-10-19 18:57:11 +0400

    ui: Disable text drag in useDragSelect

--- ui/src/lib/react/hooks/useDragSelect.tsx
+++ ui/src/lib/react/hooks/useDragSelect.tsx
@@ -2,10 +2,23 @@ import {useEffect} from "react";
 import DragSelect from "dragselect";
 
 export default function useDragSelect(
-        selectablesClassName: string,
-        areaClassName: string,
-        setSelectedIds: ((ids: string[]) => void)
+    selectablesClassName: string,
+    areaClassName: string,
+    setSelectedIds: ((ids: string[]) => void)
 ) {
+
+    /**
+     * Used to prevent chrome dragging of selected text, which leads to incorrect behaviour.
+     */
+    function clearBrowserSelection() {
+        if (!!window?.getSelection()) {
+            if (!!window?.getSelection()?.empty) {  // Chrome
+                window?.getSelection()?.empty();
+            } else if (!!window?.getSelection()?.removeAllRanges) {  // Firefox
+                window?.getSelection()?.removeAllRanges();
+            }
+        }
+    }
 
     function enableSelection(): () => void {
         try {
@@ -15,22 +28,23 @@ export default function useDragSelect(
                 draggability: false,
             } as unknown as Settings);
 
-            let itemsSelected = function() {
+            let itemsSelected = function () {
                 let selectedItems = dragselect.getSelection()
                     .map(it => it.getAttribute("data-selection-id"))
                     .filter(it => !!it) as string[];
                 setSelectedIds(selectedItems);
+                clearBrowserSelection();
                 console.log(selectedItems);
             }
             dragselect.subscribe("elementselect", itemsSelected);
-            dragselect.subscribe("elementunselect",itemsSelected);
+            dragselect.subscribe("elementunselect", itemsSelected);
             return function cleanup() {
                 dragselect.stop()
             }
         } catch (e) {
             console.log(e)
         }
-        return () => {};
+        return () => null;
     }
 
     useEffect(() => {
