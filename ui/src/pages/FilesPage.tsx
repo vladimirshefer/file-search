3b7f390e212f1d39454448ac83851c4b106e1f46
Vladimir Shefer <quameu@gmail.com>
2022-10-20 00:50:31 +0400

    ui: FilesPage: Imports optimize

--- ui/src/pages/FilesPage.tsx
+++ ui/src/pages/FilesPage.tsx
@@ -9,7 +9,7 @@ import Breadcrumbs from "components/files/BreadCrumbs";
 import DirectoryCardGrid from "components/FilesPage/DirectoryCardGrid/DirectoryCardGrid";
 import {Readme} from "components/files/Readme";
 import FilesList from "components/FilesPage/FilesList/FilesList";
-import FileApiService from "../lib/service/FileApiService";
+import FileApiService from "lib/service/FileApiService";
 import DragArea from "components/drag/DragArea";
 
 function FilesPage() {
