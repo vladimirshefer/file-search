f02703d8d3f18fb944bd529a962591575c0210d2
Vladimir Shefer <quameu@gmail.com>
2022-10-20 21:00:48 +0400

    ui: Header: Set logo text to Media

--- ui/src/components/Header/index.tsx
+++ ui/src/components/Header/index.tsx
@@ -6,7 +6,7 @@ function Header() {
     return <>
         <nav className={"header"}>
             <div className={"header_logo"}>
-                <span className={"header_logo_text"}>#</span>
+                <span className={"header_logo_text"}>Media</span>
             </div>
             <NavItem to={"/"} text={"Home"}/>
             <NavItem to={"/files"} text={"Files"}/>
