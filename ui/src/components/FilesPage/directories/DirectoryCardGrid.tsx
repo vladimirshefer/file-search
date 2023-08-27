179f09cc2782411cab1e41cdda64ca4571d9462f
Vladimir Shefer <quameu@gmail.com>
2023-08-27 20:49:23 +0400

    UI: Change directory card click handles

--- ui/src/components/FilesPage/directories/DirectoryCardGrid.tsx
+++ ui/src/components/FilesPage/directories/DirectoryCardGrid.tsx
@@ -35,42 +35,56 @@ export default function DirectoryCardGrid(
         key={name}
         className={`directory-card drag-selectable ${isSelected ? "directory-card__selected" : ""}`}
         data-selection-id={name}
+        onDoubleClick={() =>
+            navigate(`./${name}`)
+        }
+        title={name}
     >
-        <Link
-            to={"./" + name}
-            relative={"path"}
-            title={name}
-            onTouchEnd={() => {
-                navigate("./" + name)
-            }}
-        >
-            <div className="directory-card_main-line">
-                <div className={"directory-card_icon"}>
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
-                        stroke="currentColor" className="w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round"
-                              d="M2.25 12.75V12A2.25 2.25 0 014.5 9.75h15A2.25 2.25 0 0121.75 12v.75m-8.69-6.44l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z"/>
-                    </svg>
-                </div>
-                <span className={"directory-card_name"}>
-                        {name}
-                </span>
+
+        <div className="directory-card_main-line">
+            <div className={"directory-card_icon"}>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                    stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round"
+                          d="M2.25 12.75V12A2.25 2.25 0 014.5 9.75h15A2.25 2.25 0 0121.75 12v.75m-8.69-6.44l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z"/>
+                </svg>
             </div>
-        </Link>
+            <span className={"directory-card_name"}>
+                {name}
+            </span>
+        </div>
         <div className={"directory-card_status-line"}>
                 <span
                     className={"directory-card_status-item directory-card_size"}
                     onClick={(e) => {
                         e.preventDefault()
+                        e.stopPropagation()
                     }}
                     onDoubleClick={(e) => {
-                        e.preventDefault();
+                        e.preventDefault()
+                        e.stopPropagation()
                         requestSize()
                     }}
                 >
                     {ConversionUtils.getReadableSize(size)}
                 </span>
+
+            <Link
+                to={"./" + name}
+                relative={"path"}
+                title={name}
+                onTouchEnd={() => {
+                    navigate(`./${name}`)
+                }}
+            >
+                <span
+                    className={"directory-card_status-item"}
+                >
+                    {"Open"}
+                </span>
+            </Link>
+
         </div>
     </li>;
 }
